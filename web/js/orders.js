15352e737af82c17f43c61616cb69c263c0d8f0c
lexxl <shvildadze.aleksey@projektcs.com>
2016-11-11 14:44:07 +0200

    fonts added, css added, table tuned

--- web/js/orders.js
+++ web/js/orders.js
@@ -3,8 +3,36 @@
  */
 
 $(document).ready(function(){
-    $("#orders-table").dataTable({
-        "order": [[0, "desc"]]
-    } );
+	$("#orders-table").dataTable({
+		"dom": 'ftr',				// https://datatables.net/reference/option/dom
+		"order": [[0, "desc"]], 	// columns sorting
+		// "oLanguage": {
+		// 	"sSearch": ""
+		// },
+		"language": {				// search filter
+			search: "_INPUT_",
+			searchPlaceholder: "Search Orders"
+		},
+		initComplete: function () {
+			this.api().columns([1]).every( function () {
+				var column = this;
+				var select = $('<select><option value=""></option></select>')
+					.appendTo('#orders-table_filter')
+					.addClass('form-control input-sm')
+					.on( 'change', function () {
+						var val = $.fn.dataTable.util.escapeRegex(
+							$(this).val()
+						);
+ 
+						column
+							.search( val ? '^'+val+'$' : '', true, false )
+							.draw();
+					} );
+ 
+				column.data().unique().sort().each( function ( d, j ) {
+					select.append( '<option value="'+d+'">'+d+'</option>' )
+				} );
+			} );
+		}
+	});
 });
-
