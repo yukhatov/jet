71bec40288278a2eaf61e46308d548f30069cd1a
Aleksey Shvildadze <shvildadze.aleksey@projektcs.com>
2017-01-19 13:57:31 +0200

    JET-45

--- web/js/reportOrders.js
+++ web/js/reportOrders.js
@@ -27,7 +27,7 @@ $( document ).ready(function() {
 						if(isNaN(b)){ b = 0; }
 
 						return a + b;
-					});
+					}, 0);
 
 				$(column.footer()).html(sum);
 			});
@@ -79,6 +79,16 @@ $( document ).ready(function() {
 				"previous": '<i class="fa fa-3x fa-angle-left" aria-hidden="true"></i>',
 				"next": 	'<i class="fa fa-3x fa-angle-right" aria-hidden="true"></i>'
 			},
+			// processing : "<i class='fa fa-spinner fa-spin fa-3x fa-fw preloader-spinner'></i>"
+			processing : 
+				'	<div class="preloader-spinner">	'+
+				'		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="64" height="64" fill="white">	'+
+				'			<path opacity=".2" d="M16 0 A16 16 0 0 0 16 32 A16 16 0 0 0 16 0 M16 4 A12 12 0 0 1 16 28 A12 12 0 0 1 16 4"/>	'+
+				'			<path d="M16 0 A16 16 0 0 1 32 16 L28 16 A12 12 0 0 0 16 4z">	'+
+				'				<animateTransform attributeName="transform" type="rotate" from="0 16 16" to="360 16 16" dur="0.8s" repeatCount="indefinite" />	'+
+				'			</path>	'+
+				'		</svg>	'+
+				'	</div>	'
 		},
 		'footerCallback': function (){
 			// calculating sums
